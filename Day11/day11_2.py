a8911505acfa659cc8991a92b9879d8aa0daebce
dichohnf <dicho@hotmail.it>
2024-12-13 22:38:19 +0100

    Day11 part 2

--- Day11/day11_2.py
+++ Day11/day11_2.py
@@ -1,38 +1,19 @@
-from collections import deque
-import multiprocessing as mp
+rocks = open("../inputs/11.in").read().splitlines()[0].split()
+rocks = {rock:rocks.count(rock) for rock in rocks}
 
-def process_rock(rock, iterations):
-    queue = deque([(rock, 0)])
-    count = 0
+def calc_next(rock):
+        if len(rock) % 2 == 0:
+            return [rock[:len(rock)//2], str(int(rock[len(rock)//2:]))]
+        return '1' if rock == '0' else str(int(rock) * 2024)
 
-    while queue:
-        current_rock, current_iteration = queue.popleft()
-        if current_iteration == iterations:
-            count += 1
+for _ in range(75):
+    next_rocks = {}
+    for rock in rocks:
+        next_rock = calc_next(rock)
+        if isinstance(next_rock, str):
+            next_rocks[next_rock] = next_rocks[next_rock] + rocks[rock] if next_rock in next_rocks else rocks[rock]
         else:
-            if current_rock == '0':
-                queue.append(('1', current_iteration + 1))
-            elif len(current_rock) % 2 == 0:
-                mid = len(current_rock) // 2
-                left_half = current_rock[:mid]
-                right_half = str(int(current_rock[mid:]))
-                queue.append((left_half, current_iteration + 1))
-                queue.append((right_half, current_iteration + 1))
-            else:
-                new_rock = str(int(current_rock) * 2024)
-                queue.append((new_rock, current_iteration + 1))
-
-    return count
-
-
-def main():
-    rocks = open("../inputs/11.in").read().splitlines()[0].split()
-    iterations = 75
-    max_processes = 16
-
-    with mp.Pool(processes=min(max_processes, len(rocks))) as pool:
-        results = pool.starmap(process_rock, [(rock, iterations) for rock in rocks])
-    print(sum(results))
-
-if __name__ == "__main__":
-    main()
+            for r in next_rock:
+                next_rocks[r] = next_rocks[r] + rocks[rock] if r in next_rocks else rocks[rock]
+    rocks = next_rocks
+print(sum(rocks.values()))
